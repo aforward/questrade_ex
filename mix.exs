b38575b2320e1794ff03c756ced54cc6ae042cd6
Andrew Forward <aforward@gmail.com>
2018-03-24 15:16:33 -0400

    Update httpoison to latest

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule QuestradeEx.Mixfile do
   @deps [
     {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
     {:jason, "~> 1.0"},
-    {:httpoison, "~> 0.11.1"},
+    {:httpoison, "~> 1.0"},
     {:fn_expr, "~> 0.2"},
     {:version_tasks, "~> 0.10"},
     {:ex_doc, ">= 0.0.0", only: :dev}
