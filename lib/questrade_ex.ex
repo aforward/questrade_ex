20facb0112569029201bff1bae2fe4931c734223
Andrew Forward <aforward@hey.com>
2022-03-20 17:30:41 -0400

    Re-working API to not rely on stored tokens

--- lib/questrade_ex.ex
+++ lib/questrade_ex.ex
@@ -56,47 +56,91 @@ defmodule QuestradeEx do
 
   """
 
-  @doc """
-  Seed questrade with a manual refresh token.
+  defdelegate access_token(token), to: QuestradeEx.Client
 
-  ## Examples
-
-      QuestradeEx.seed_token("me", "abc123")
-  """
-  defdelegate seed_token(user, refresh_token), to: QuestradeEx.Client, as: :fetch_token
+  defdelegate refresh_token(token), to: QuestradeEx.Client
 
   @doc """
-  Grab the token for the user
+  See the underlying client method for more details.
 
-  ## Examples
+  To extract your account information
 
-      QuestradeEx.fetch_token("me")
+  ```elixir
+  QuestradeEx.request(token, :get, resource: "v1/accounts")
+  ```
+
+  To lookup companies
+
+  ```elixir
+  QuestradeEx.request(token, :get, resource: "v1/symbols/search", params: [prefix: "AAPL"])
+  ```
+
+  With a sample reply like:
+
+  ```elixir
+  %{
+       currency: "USD",
+       description: "APPLE INC",
+       isQuotable: true,
+       isTradable: true,
+       listingExchange: "NASDAQ",
+       securityType: "Stock",
+       symbol: "AAPL",
+       symbolId: 8049
+     }
+  ```
+
+
+  To lookup a specific symbol
+
+  ```elixir
+  QuestradeEx.request(token, :get, resource: "v1/symbols/8049")
+  ```
+
+  With a sample reply like
+
+  ```elixir
+  %{
+   symbols: [
+     %{
+       pe: 26.63682,
+       isTradable: true,
+       industrySector: "Technology",
+       minTicks: [%{minTick: 1.0e-4, pivot: 0}, %{minTick: 0.01, pivot: 1}],
+       tradeUnit: 1,
+       averageVol3Months: 92733375,
+       currency: "USD",
+       optionStrikePrice: nil,
+       highPrice52: 182.94,
+       optionExpiryDate: nil,
+       dividend: 0.22,
+       industrySubgroup: "Undefined",
+       hasOptions: true,
+       isQuotable: true,
+       industryGroup: "Undefined",
+       listingExchange: "NASDAQ",
+       optionRoot: "",
+       symbolId: 8049,
+       marketCap: 2621228613400,
+       yield: 0.54788,
+       lowPrice52: 118.86,
+       optionExerciseType: nil,
+       dividendDate: "2022-02-10T00:00:00.000000-05:00",
+       optionDurationType: nil,
+       prevDayClosePrice: 163.98,
+       description: "APPLE INC",
+       exDate: "2022-02-04T00:00:00.000000-05:00",
+       optionContractDeliverables: %{cashInLieu: 0, underlyings: []},
+       averageVol20Days: 97763442,
+       securityType: "Stock",
+       symbol: "AAPL",
+       optionType: nil,
+       outstandingShares: 16319440000,
+       eps: 6.03
+     }
+   ]
+  }
+  ```
   """
-  defdelegate fetch_token(user), to: QuestradeEx.Client, as: :fetch_token
-
-  @doc """
-  Issues an HTTP request with the given method to the given url_opts.
-
-  Args:
-    * `user` - Which user is making the request
-    * `method` - HTTP method as an atom (`:get`, `:head`, `:post`, `:put`, `:delete`, etc.)
-    * `opts` - A keyword list of options to help create the URL, provide the body and/or query params
-
-  The options above can be defaulted using `Mix.Config` configurations,
-  as documented above.
-
-  This function returns `{<status_code>, response}` if the request is successful, and
-  `{:error, reason}` otherwise.
-
-  ## Examples
-
-      QuestradeEx.request("me", :get, resource: "domains")
-
-  """
-  defdelegate request(user, method, opts \\ []), to: QuestradeEx.Client
-
-  @doc """
-  Lookup a symbol.
-  """
-  defdelegate lookup_symbol(user, symbol), to: QuestradeEx.Resources.Search
+  defdelegate request(token, method, base_opts \\ []), to: QuestradeEx.Client
 end
--- lib/questrade_ex/client.ex
+++ lib/questrade_ex/client.ex
@@ -1,4 +1,6 @@
 defmodule QuestradeEx.Client do
+  alias QuestradeEx.Api
+
   @moduledoc """
   Helper functions to access the QuestradeEx API in a
   more succinct way.  If any features of the API are
@@ -34,91 +36,144 @@ defmodule QuestradeEx.Client do
   them with `Mix.Config` to avoid using them throughout your code.
   """
 
-  use FnExpr
-  alias QuestradeEx.{Api, Worker}
+  @doc """
+  Extract the access_token from the user.
+
+  If you provide a string (binary) then that means you manually
+  created one from the Questrade UI
+  https://apphub.questrade.com/UI/UserApps.aspx
+
+  For example,
+
+  ```elixir
+  QuestradeEx.Client.access_token("wmABCKacPC5MCwT-DefaqYKQloH123ZyR0")
+  ```
+
+  It will return the access_token, if generated
+
+  ```elixir
+  %{
+    access_token: "abcvwakdefDxYxYnhijl9Tq90210yj344j0",
+    api_server: "https://api07.iq.questrade.com/",
+    expires_in: 1800,
+    refresh_token: "abc6V7defRIdhijLO5123Q1j90210cK0P70",
+    token_type: "Bearer"
+  }
+  ```
+
+  If that token is no longer valid, the reply will look like
+
+  ```
+  nil
+  ```
+
+  If you already have the full token, it will return what
+  you provided :-)
+  """
+  def access_token(token) when is_map(token), do: token
+
+  def access_token(refresh_token), do: refresh_token(refresh_token)
 
   @doc """
-  Make an API call, possibly requiring a refreshed token
-  """
-  def request(user, method, opts) do
-    user
-    |> request_once(method, opts)
-    |> request_retry(user, method, opts)
-  end
+  Refresh the access token.  If you provide a string, that
+  means you created one manually.
 
-  @doc """
-  Make an API call to questrade for the provided user using the available token
-  """
-  def request_once(user, method, opts) do
-    user
-    |> fetch_token
-    |> do_request(method, opts)
-  end
+  ```elixir
+  Token.refresh_token("wmABCKacPC5MCwT-DefaqYKQloH123ZyR0")
+  ```
 
-  @doc """
-  Interpret the results of an API request, and possibly retry if we encounter
-  something that is retryable
-  """
-  def request_retry({401, %{code: 1017}}, user, method, opts) do
-    user
-    |> refresh_token
-    |> do_request(method, opts)
-  end
+  If you have a _now_ expired token, then you will need to
+  extract the refresh_token from the token.
 
-  def request_retry(other_response, _user, _method, _opts), do: other_response
+  ```elixir
+  Token.refresh_token(%{refresh_token: "wmABCKacPC5MCwT-DefaqYKQloH123ZyR0"})
+  ```
 
-  @doc """
-  Fetch a token based on the provided refresh_token for the user
+  The results will look similar as &access_token/1
   """
-  def fetch_token(user, refresh_token) do
+  def refresh_token(%{refresh_token: refresh_token}), do: refresh_token(refresh_token)
+
+  def refresh_token(refresh_token) when is_binary(refresh_token) do
     Api.request(
       :post,
       resource: "/token",
       body: %{grant_type: :refresh_token, refresh_token: refresh_token},
       headers: [{"Content-Type", "application/x-www-form-urlencoded"}]
     )
-    |> assign_token(user)
-  end
-
-  @doc """
-  Fetch the stored token for the user, if none return an error
-  """
-  def fetch_token(user) do
-    case Worker.fetch_token(user) do
-      nil -> {:error, :missing_token}
-      token -> {:ok, token}
-    end
-  end
-
-  @doc """
-  Refresh the token
-  """
-  def refresh_token(user) do
-    user
-    |> fetch_token
     |> case do
-      {:ok, %{refresh_token: refresh_token}} -> fetch_token(user, refresh_token)
-      reply -> reply
+      {200, token} -> token
+      _error -> nil
     end
   end
 
   @doc """
-  Maybe you have a legit token, here's how how you can set it directly
-  """
-  def assign_token({200, token}, user), do: {:ok, Worker.assign_token(user, token)}
-  def assign_token({_, reason}, _), do: {:error, reason}
-  def assign_token(token, user), do: Worker.assign_token(user, token)
+  Once you have a token, you can make authenticated requests, such as
 
-  defp do_request(
-         {:ok, %{api_server: base_url, access_token: token, token_type: "Bearer"}},
-         method,
-         opts
-       ) do
-    opts
-    |> Keyword.put(:base, base_url)
-    |> Keyword.put(:bearer_auth, token)
-    |> invoke(Api.request(method, &1))
+  ```elixir
+  QuestradeEx.authenticated_request(token, :get, resource: "v1/accounts")
+  ```
+
+  Which will return something like
+
+  ```elixir
+  {200,
+   %{
+     accounts: [
+       %{
+         clientAccountType: "Individual",
+         isBilling: true,
+         isPrimary: true,
+         number: "55511111",
+         status: "Active",
+         type: "TFSA"
+       },
+       %{
+         clientAccountType: "Individual",
+         isBilling: false,
+         isPrimary: false,
+         number: "444222222",
+         status: "Active",
+         type: "RRSP"
+       }
+     ],
+     userId: 90210
+   }}
+  ```
+  """
+  def request(token, method), do: request(token, method, [])
+
+  def request(token, method, base_opts) when is_binary(token) do
+    new_token = access_token(token)
+    {ok, answer, _} = request(new_token, method, base_opts)
+    {ok, answer, new_token}
   end
 
-  defp do_request(other_response, _method, _opts), do: other_response
+  def request(token, method, base_opts) when is_map(token) do
+    token
+    |> call_once(method, base_opts)
+    |> case do
+      {:error, {401, %{code: 1017}}, bad_token} ->
+        bad_token
+        |> refresh_token()
+        |> call_once(method, base_opts)
+
+      answer ->
+        answer
+    end
+  end
+
+  defp call_once(token, method, base_opts) do
+    opts =
+      base_opts
+      |> Keyword.put(:base, token[:api_server])
+      |> Keyword.put(:bearer_auth, token[:access_token])
+
+    expected_code = Keyword.get(opts, :expected_code, 200)
+
+    Api.request(method, opts)
+    |> case do
+      {^expected_code, good_answer} -> {:ok, good_answer, token}
+      bad_answer -> {:error, bad_answer, token}
+    end
+  end
 end
