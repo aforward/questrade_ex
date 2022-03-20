d68bba18b71a018f9c87ec52656f26a84f6b2a65
Andrew Forward <aforward@hey.com>
2022-03-20 17:54:34 -0400

    Upgrade project template and drop application

--- mix.exs
+++ mix.exs
@@ -1,41 +1,32 @@
-defmodule QuestradeEx.Mixfile do
+defmodule QuestradeEx.MixProject do
   use Mix.Project
 
-  @name :questrade_ex
-  @version "0.2.0"
-
-  @deps [
-    {:jason, "~> 1.3"},
-    {:httpoison, "~> 1.3"},
-    {:fn_expr, "~> 0.3"},
-    {:version_tasks, "~> 0.12"},
-    {:persistent_ets, github: "michalmuskala/persistent_ets"},
-    {:ex_doc, ">= 0.0.0", only: :dev}
-  ]
-
-  @aliases []
-
-  # ------------------------------------------------------------
-
   def project do
-    in_production = Mix.env() == :prod
-
     [
-      app: @name,
-      version: @version,
+      app: :questrade_ex,
+      version: "0.2.0",
       elixir: "~> 1.13",
-      deps: @deps,
-      aliases: @aliases,
-      build_embedded: in_production
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
     ]
   end
 
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {QuestradeEx.Application, []},
-      extra_applications: [
-        :logger
-      ]
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:jason, "~> 1.3"},
+      {:httpoison, "~> 1.3"},
+      {:fn_expr, "~> 0.3"},
+      {:version_tasks, "~> 0.12"},
+      {:persistent_ets, github: "michalmuskala/persistent_ets"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
