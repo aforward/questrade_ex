803d4275d9ec29f41e135f9ebc2daf2bb76c1bc3
Andrew Forward <aforward@hey.com>
2022-03-20 13:36:30 -0400

    Enable encrypt / decrypt of settings

--- test/settings_test.exs
+++ test/settings_test.exs
@@ -13,6 +13,43 @@ defmodule QuestradeEx.SettingsTest do
     end
   end
 
+  describe "encrypt/2" do
+    test "based on the secret" do
+      pid1 = settings(secret: "shhh1")
+      pid2 = settings(secret: "shhh2")
+
+      encrypted1a = Settings.encrypt("hello", pid1)
+      encrypted1b = Settings.encrypt("hello", pid1)
+      assert encrypted1a != encrypted1b
+
+      encrypted2a = Settings.encrypt("hello", pid2)
+      encrypted2b = Settings.encrypt("hello", pid2)
+      assert encrypted2a != encrypted2b
+    end
+  end
+
+  describe "decrypt/2" do
+    test "based on the secret" do
+      pid1 = settings(secret: "shhh1")
+      pid2 = settings(secret: "shhh2")
+
+      encrypted1 = Settings.encrypt("hello", pid1)
+      assert Settings.decrypt(encrypted1, pid1) == {:ok, "hello"}
+
+      encrypted2 = Settings.encrypt("hello", pid2)
+      assert Settings.decrypt(encrypted2, pid2) == {:ok, "hello"}
+    end
+
+    test "handles invalid challenges with nil" do
+      pid1 = settings(secret: "shhh1")
+      pid2 = settings(secret: "shhh2")
+
+      encrypted1 = Settings.encrypt("hello", pid1)
+      {:error, message} = Settings.decrypt(encrypted1, pid2)
+      assert message =~ "Unable to decrypt"
+    end
+  end
+
   def settings(opts \\ []) do
     opts
     |> Keyword.put(:name, "t#{:rand.uniform(100_000)}" |> String.to_atom())
